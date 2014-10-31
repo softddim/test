3883496f32c2c6e6b2c900c84315e6f7c7f92d12
yudong <1223277987@qq.com>
2014-10-31 15:39:02 +0800

    添加了一个test类

--- src/DDD.java
+++ src/DDD.java
@@ -8,6 +8,8 @@ public class DDD {
 	public void print(){
 		System.out.println("i want to test the refresh..");
 	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("这是第一个git项目");
 	}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+/**
+ *@author: yudong
+ *@data:2014-10-31 обнГ3:38:37
+ *@function:
+ **/
+public class Test {
+
+}
