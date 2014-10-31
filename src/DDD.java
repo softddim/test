bfafb039f7b5837e3ed79dc2e8d0cc8c8c22ed67
yudong <1223277987@qq.com>
2014-10-31 15:41:19 +0800

    添加了一个重载方法print(str)

--- src/DDD.java
+++ src/DDD.java
@@ -8,6 +8,10 @@ public class DDD {
 	public void print(){
 		System.out.println("i want to test the refresh..");
 	}
+	
+	public void print(String srt){
+		System.out.println(srt);
+	}
 	public static void main(String[] args) {
 		System.out.println("这是第一个git项目");
 	}
